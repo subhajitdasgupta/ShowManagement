17a1f623f0ddb802dc3fd614eb6db84c05cb8904
Subhajit Dasgupta <dasguptasubhajitbally@gmail.com>
2023-08-02 16:42:34 +0530

    Adding Database conncetion & separating the application db context.

---
+++ ShowManagement/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ShowManagement.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("GigHubConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
